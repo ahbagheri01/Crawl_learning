9ceb1e4de394dfc5ff1f38531617123bb2956a5c
amirhosseinbagheri <ahbagheri01@gmail.com>
2021-12-31 12:36:43 +0330

    only first one

--- lr/lr/spiders/Ganjoor_spider.py
+++ lr/lr/spiders/Ganjoor_spider.py
@@ -8,10 +8,29 @@ class Ganjoor_spider(scrapy.Spider):
    # def parse(self, response, **kwargs):
      #   return super().parse(response, **kwargs)
 
-    def parse(self, response,** kwargs):
-        title = response.css('title::text').extract()
-        yield {
-            'title':title
+    def parse(self, response):
+        all_div_cols = response.css('div.quote')[0]
+        title = all_div_cols.css('span.text::text').extract()
+        author= all_div_cols.css('.author::text').extract()
+        teg = all_div_cols.css('.tag::text').extract()
+        yield{
+            'title':title,
+            'authos':author,
+            'tag':teg
         }
-    
 
+
+
+# scrapy shell 'https://quotes.toscrape.com/'
+#response.css("title::text").extract_first()
+# https://quotes.toscrape.com/
+#response.css("span.text::text")[0].extract()  for class . 
+#response.css("span#text::text")[0].extract()  for id #
+#  selector gadget chrome
+#response.css(".author::text")[0].extract()
+# Xpath selectors
+#response.xpath("//title/text()").extract()
+# response.xpath("//span[@class = 'text']/text()").extract()
+#response.xpath("//span[@class = 'text']/text()")[0].extract()
+# response.css("li.next a").xpath("@href").extract()
+# response.css("a").xpath("@href").extract()
